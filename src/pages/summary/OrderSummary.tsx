967e3c21a577216ed2807163d78d9cce50b551bf
Francisco <javiermartincuenca@gmail.com>
2021-06-05 11:09:56 +0200

    Not show toppings if it is not ordered

--- src/pages/summary/OrderSummary.tsx
+++ src/pages/summary/OrderSummary.tsx
@@ -10,23 +10,32 @@ interface OrderSummaryProps {
 const OrderSummary: React.FC<OrderSummaryProps> = ({ setOrderPhase }) => {
   const [orderDetails] = useOrderDetails();
 
-  const scoopArray = Array.from(orderDetails.scoops.entries());
-  const scoopList = scoopArray.map(([key, value]) => (
-    <li key={key}>
-      {value} {key}
-    </li>
-  ));
+  const scoopList = Array.from(orderDetails.scoops.entries()).map(
+    ([key, value]) => (
+      <li key={key}>
+        {value} {key}
+      </li>
+    )
+  );
 
-  const toppingsArray = Array.from(orderDetails.toppings.entries());
-  const toppingList = toppingsArray.map(([key]) => <li key={key}>{key}</li>);
+  const toppingsDisplay =
+    orderDetails.toppings.size > 0 ? (
+      <>
+        <h2>Toppings: {orderDetails.totals.toppings}</h2>
+        <ul>
+          {Array.from(orderDetails.toppings.entries()).map(([key]) => (
+            <li key={key}>{key}</li>
+          ))}
+        </ul>
+      </>
+    ) : null;
 
   return (
     <div>
       <h1>Order Summary</h1>
       <h2>Scoops: {orderDetails.totals.scoops}</h2>
       <ul>{scoopList}</ul>
-      <h2>Toppings: {orderDetails.totals.toppings}</h2>
-      <ul>{toppingList}</ul>
+      {toppingsDisplay}
       <SummaryForm setOrderPhase={setOrderPhase} />
     </div>
   );
